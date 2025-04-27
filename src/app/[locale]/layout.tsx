c9eb9b109518fb7e6e9c8f67de38ad5126e9a570
Vlad Olkinyan <vladolkinyan@Vlads-MacBook-Pro.local>
2025-04-27 19:37:04 +0400

    fix

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -43,7 +43,7 @@ export default async function LocaleLayout({
   params,
 }: {
   children: React.ReactNode;
-  params: { locale: string };
+  params: any;
 }) {
   const { locale } = await params;
   if (!hasLocale(routing.locales, locale)) {
